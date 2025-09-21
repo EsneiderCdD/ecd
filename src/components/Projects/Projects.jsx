b7f3442fa7d72f088e47beb7dc2ef9715b4df9f1
EsneiderCdD <ecd.97@hotmail.com>
2025-09-21 02:44:30 -0500

    feat: Projects

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,16 +1,21 @@
 // src/pages/Project/Project.jsx
 import AboutHeader from "../About/AboutHeader";
 import AboutSidebar from "../About/AboutSidebar";
-import FileTable from "../common/FileTable"; 
+import FileTable from "../common/FileTable";
 import AboutInfoPanel from "../About/AboutInfoPanel";
 import Taskbar from "../Taskbar/Taskbar";
 import styles from "../../components/About/About.module.css";
 
 
 const desktopFiles = [
-  { icon: "📁", name: "archivos", type: "Carpeta", date: "20/09/2025 9:06 p.m.", size: "-" },
-  { icon: "📁", name: "archivos", type: "Carpeta", date: "11/09/2025 12:47 a.m.", size: "-" },
-  { icon: "📄", name: "archivos", type: "Acceso directo", date: "25/02/2025 6:53 p.m.", size: "3 KB" },
+  { icon: "📁", name: "MartinaClandestina", type: "Carpeta", date: "20/09/2025 9:06 p.m.", size: "-" },
+  { icon: "📁", name: "MisterSoldo", type: "Carpeta", date: "11/09/2025 12:47 a.m.", size: "-" },
+  { icon: "📁", name: "@Psicotranspersonal", type: "Carpeta", date: "20/09/2025 9:06 p.m.", size: "-" },
+  { icon: "📁", name: "GUAU", type: "Carpeta", date: "11/09/2025 12:47 a.m.", size: "-" },
+  { icon: "📁", name: "FramerMotionLab", type: "Carpeta", date: "11/09/2025 12:47 a.m.", size: "-" },
+  { icon: "📁", name: "Trainit Trello", type: "Carpeta", date: "11/09/2025 12:47 a.m.", size: "-" },
+  { icon: "📁", name: "Postulamania", type: "Carpeta", date: "11/09/2025 12:47 a.m.", size: "-" },
+  { icon: "📄", name: "nn", type: "Acceso directo", date: "25/02/2025 6:53 p.m.", size: "3 KB" },
 ];
 
 function Project() {
