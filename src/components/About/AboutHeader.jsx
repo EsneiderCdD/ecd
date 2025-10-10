936a7befc8877fe6daa72979baceb6b734cf61b1
EsneiderCdD <ecd.97@hotmail.com>
2025-10-10 09:15:08 -0500

    feat: dropdown y estilos

--- src/components/About/AboutHeader.jsx
+++ src/components/About/AboutHeader.jsx
@@ -1,11 +1,8 @@
 import styles from "./About.module.css";
 import {
-  Edit3,      // Cambiar nombre
-  Share2,     // Compartir
-  Trash2,     // Eliminar
-  ArrowUpDown,// Ordenar
-  Layout,     // Ver
-  List,       // Detalles
+  ArrowUpDown, // Ordenar
+  Layout,      // Ver
+  List,        // Detalles
 } from "lucide-react";
 
 import Dropdown from "./Dropdown/Dropdown";
@@ -42,10 +39,29 @@ function AboutHeader() {
           className={styles.icon}
         />
 
-        {/* Mantienes los demás */}
-        <Edit3 className={styles.icon} title="Cambiar nombre" />
-        <Share2 className={styles.icon} title="Compartir" />
-        <Trash2 className={styles.icon} title="Eliminar" />
+        {/* ❤️ Favorito */}
+        <img
+          src="https://cdn-icons-png.flaticon.com/512/833/833472.png"
+          alt="Favorito"
+          title="Favorito"
+          className={styles.icon}
+        />
+
+        {/* 📤 Compartir */}
+        <img
+          src="https://cdn-icons-png.flaticon.com/512/2958/2958783.png"
+          alt="Compartir"
+          title="Compartir"
+          className={styles.icon}
+        />
+
+        {/* 💌 Feedback */}
+        <img
+          src="https://cdn-icons-png.flaticon.com/512/2099/2099199.png"
+          alt="Feedback"
+          title="Feedback"
+          className={styles.icon}
+        />
       </div>
 
       {/* Ordenar */}
@@ -58,6 +74,66 @@ function AboutHeader() {
       <div className={styles.icons4}>
         <Layout className={styles.icon} title="Ver" />
         <p>Ver</p>
+      </div>
+
+      {/* ✅ BLOQUE: STACK */}
+      <div className={styles.iconsStack}>
+        <p>Stack</p>
+
+        <img
+          src="https://cdn-icons-png.flaticon.com/512/5968/5968292.png"
+          alt="JavaScript"
+          title="JavaScript"
+          className={styles.icon}
+        />
+        <img
+          src="https://cdn-icons-png.flaticon.com/512/919/919851.png"
+          alt="React"
+          title="React"
+          className={styles.icon}
+        />
+        <img
+          src="https://cdn-icons-png.flaticon.com/512/919/919827.png"
+          alt="HTML"
+          title="HTML"
+          className={styles.icon}
+        />
+        <img
+          src="https://cdn-icons-png.flaticon.com/512/5968/5968242.png"
+          alt="CSS"
+          title="CSS"
+          className={styles.icon}
+        />
+        <img
+          src="https://cdn-icons-png.flaticon.com/512/2111/2111288.png"
+          alt="Git"
+          title="Git"
+          className={styles.icon}
+        />
+        <img
+          src="https://cdn-icons-png.flaticon.com/512/733/733609.png"
+          alt="GitHub"
+          title="GitHub"
+          className={styles.icon}
+        />
+        <img
+          src="https://cdn-icons-png.flaticon.com/512/174/174857.png"
+          alt="LinkedIn"
+          title="LinkedIn"
+          className={styles.icon}
+        />
+        <img
+          src="https://cdn-icons-png.flaticon.com/512/5968/5968381.png"
+          alt="Next.js"
+          title="Next.js"
+          className={styles.icon}
+        />
+        <img
+          src="https://cdn-icons-png.flaticon.com/512/5968/5968342.png"
+          alt="PostgreSQL"
+          title="PostgreSQL"
+          className={styles.icon}
+        />
       </div>
 
       {/* Detalles */}
