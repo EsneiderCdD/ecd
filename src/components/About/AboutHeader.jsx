9911c6fbd543e4437403fdd71c3758506ae736a1
EsneiderCdD <ecd.97@hotmail.com>
2025-10-12 08:25:29 -0500

    chore: clean stack tecnologico

--- src/components/About/AboutHeader.jsx
+++ src/components/About/AboutHeader.jsx
@@ -76,66 +76,6 @@ function AboutHeader() {
         <p>Ver</p>
       </div>
 
-      {/* ✅ BLOQUE: STACK */}
-      <div className={styles.iconsStack}>
-        <p>Stack</p>
-
-        <img
-          src="https://cdn-icons-png.flaticon.com/512/5968/5968292.png"
-          alt="JavaScript"
-          title="JavaScript"
-          className={styles.icon}
-        />
-        <img
-          src="https://cdn-icons-png.flaticon.com/512/919/919851.png"
-          alt="React"
-          title="React"
-          className={styles.icon}
-        />
-        <img
-          src="https://cdn-icons-png.flaticon.com/512/919/919827.png"
-          alt="HTML"
-          title="HTML"
-          className={styles.icon}
-        />
-        <img
-          src="https://cdn-icons-png.flaticon.com/512/5968/5968242.png"
-          alt="CSS"
-          title="CSS"
-          className={styles.icon}
-        />
-        <img
-          src="https://cdn-icons-png.flaticon.com/512/2111/2111288.png"
-          alt="Git"
-          title="Git"
-          className={styles.icon}
-        />
-        <img
-          src="https://cdn-icons-png.flaticon.com/512/733/733609.png"
-          alt="GitHub"
-          title="GitHub"
-          className={styles.icon}
-        />
-        <img
-          src="https://cdn-icons-png.flaticon.com/512/174/174857.png"
-          alt="LinkedIn"
-          title="LinkedIn"
-          className={styles.icon}
-        />
-        <img
-          src="https://cdn-icons-png.flaticon.com/512/5968/5968381.png"
-          alt="Next.js"
-          title="Next.js"
-          className={styles.icon}
-        />
-        <img
-          src="https://cdn-icons-png.flaticon.com/512/5968/5968342.png"
-          alt="PostgreSQL"
-          title="PostgreSQL"
-          className={styles.icon}
-        />
-      </div>
-
       {/* Detalles */}
       <div className={styles.icons5}>
         <List className={styles.icon} title="Detalles" />
