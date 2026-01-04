d6ed6930cf50b128823e01c9ccb6954deb54abd2
EsneiderCdD <ecd.97@hotmail.com>
2026-01-04 13:18:29 -0500

    feature(projects): Portafolio

--- src/data/projects/portafolio.js
+++ src/data/projects/portafolio.js
@@ -34,44 +34,7 @@ export const portafolioData = {
             size: "Actualizado",
             description: "Ficha técnica en PDF.",
             previewUrl: "https://cdn-icons-png.flaticon.com/512/337/337946.png",
-            linkUrl: "https://drive.google.com/file/d/1yTCNzHmn36eF3XeRi-UAtqDe3Vre3qNc/view?usp=drive_link",
-        },
-        {
-            icon: "📋",
-            name: "Contribuciones",
-            type: "Dinámico",
-            date: "01/11/2025",
-            size: "Actualizado",
-            description: "Navega por mis principales contribuciones y aportes al proyecto TrainIT",
-            previewUrl: null,
-            downloadUrl: null,
-            linkUrl: null,
-            contributions: [
-                {
-
-                    name: "Arquitectura Frontend y Componentes",
-                    description: ["⦿ Desempeñe un rol FullStack. Asimismo, participe en establecer la **estructuración de carpetas**, la **configuración de librerías** y **patrón de diseño**.",
-                        "⦿ Implemente la estrategia ***Design System Color*** diseñado por el equipo UX/UI (colores primitivos, neutros, y semánticos) e integrándolo a Tailwind CSS.",
-                        "⦿ Construí **componentes** Integrando colores, iconos, imágenes, posiciones, fondos, botones, tipografía fehacientemente al diseño en **Figma.**"
-                    ],
-                    previewUrl: null,
-                },
-                {
-
-                    name: "Desarrollo Full-Stack",
-                    description: ["⦿ Continué desarrollando componentes del Front, opciones *(dropdown)*, enlaces, y otros detalles complementarios al dashboard.",
-                        "⦿ Realice el módulo de edición de tablero realizando ajustes en el **modelo**, **migraciones**, **lógica** de campo de la interfaz (backend) y generar la conexión desde el *Frontend* con sus respectivos componentes"],
-
-                    previewUrl: null,
-                },
-                {
-
-                    name: "Drag & Drop",
-                    description: ["⦿ Implementé el sistema ***kanban*** usando ***@dnd-kit/core***, optimizando el flujo de datos mediante estrategias de *sortable context*, *collision detection*  y render condicional con ***React.memo*** para minimizar re-renders durante operaciones CRUD.",
-                        "⦿ Aportando coherencia desde el back para la gestión de columnas y posiciones, desplazamiento entre tarjetas y entre columnas, ajustando y reordenando según la zona droppeada."],
-                    previewUrl: null,
-                }
-            ]
+            linkUrl: "https://drive.google.com/file/d/15jmB_Gu39xoOJU2SUZ4IuhIYp0QIkFxn/view?usp=drive_link",
         },
         {
             icon: "💻",
@@ -79,7 +42,7 @@ export const portafolioData = {
             type: "Imagen y texto",
             date: "01/11/2025",
             size: "Actualizado",
-            description: generateStackHTML(['git', 'github', 'figma', 'typescript', 'tailwind', 'datefns', 'next', 'react', 'zustand', 'dndKit', 'python', 'postgresql', 'flask', 'flasgger', 'pusher', 'cloudinary', 'sendgrid',]),
+            description: generateStackHTML(['vite','react','javascript','css','emailjs','uiverse','flaticon','gemini']),
             previewUrl: null,
             downloadUrl: null,
             linkUrl: null,
@@ -96,11 +59,8 @@ export const portafolioData = {
             downloadUrl: null,
             links: [
                 { label: "Frontend", url: "https://github.com/Programa-TrainIT-Ar/fe-trelloop-grupo-2/tree/grupo-2" },
-                { label: "Backend", url: "https://github.com/Programa-TrainIT-Ar/be-trelloop-grupo-2/tree/grupo-2" }
             ],
         },
-
-
     ]
 
 };
--- src/data/techStack/techStack.js
+++ src/data/techStack/techStack.js
@@ -4,11 +4,14 @@ import css from "@/assets/stack/css.webp";
 import datefns from "@/assets/stack/datefns.png";
 import dndkit from "@/assets/stack/dndkit.png";
 import docker from "@/assets/stack/docker.png";
+import emailjs from "@/assets/stack/emailjs.png";
 import express from "@/assets/stack/expressjs.webp";
 import figma from "@/assets/stack/figma.png";
 import flasgger from "@/assets/stack/flasgger.png";
 import flask from "@/assets/stack/flask.png";
+import flaticon from "@/assets/stack/flaticon.png";
 import framermotion from "@/assets/stack/framermotion.png";
+import gemini from "@/assets/stack/gemini.png";
 import git from "@/assets/stack/git.png";
 import github from "@/assets/stack/github.webp";
 import googleanalytics from "@/assets/stack/googleanalytics.webp";
@@ -16,6 +19,7 @@ import js from "@/assets/stack/js.webp";
 import mobiledesign from "@/assets/stack/mobiledesing.webp";
 import next from "@/assets/stack/next.png";
 import nodejs from "@/assets/stack/nodejs.webp";
+import openai from "@/assets/stack/openai.png";
 import payu from "@/assets/stack/payu.webp";
 import postgresql from "@/assets/stack/postgresql.png";
 import pusher from "@/assets/stack/pusher.png";
@@ -26,6 +30,7 @@ import sendgrid from "@/assets/stack/sendgrid.png";
 import swiperjs from "@/assets/stack/swiper.webp";
 import tailwind from "@/assets/stack/tailwild.png";
 import ts from "@/assets/stack/ts.png";
+import uiverse from "@/assets/stack/uiverse.jpg";
 import vite from "@/assets/stack/vite.webp";
 import zustand from "@/assets/stack/zustand.png";
 
@@ -37,17 +42,21 @@ export const stackIcons = {
   datefns: { src: datefns, alt: "Date-fns", title: "Date-fns", size: 48 },
   dndKit: { src: dndkit, alt: "DnD Kit", title: "DnD Kit", size: 48 },
   docker: { src: docker, alt: "Docker", title: "Docker", size: 58 },
+  emailjs: { src: emailjs, alt: "EmailJS", title: "EmailJS", size: 48 },
   express: { src: express, alt: "Express.js", title: "Express.js", size: 54 },
   figma: { src: figma, alt: "Figma", title: "Figma", size: 48 },
   flasgger: { src: flasgger, alt: "Flasgger", title: "Flasgger", size: 48 },
   flask: { src: flask, alt: "Flask", title: "Flask", size: 48 },
+  flaticon: { src: flaticon, alt: "Flaticon", title: "Flaticon", size: 52 },
   framermotion: { src: framermotion, alt: "Framer Motion", title: "Framer Motion", size: 48 },
+  gemini: { src: gemini, alt: "Google Gemini", title: "Google Gemini", size: 54 },
   git: { src: git, alt: "Git", title: "Git", size: 48 },
   github: { src: github, alt: "GitHub", title: "GitHub", size: 48 },
   javascript: { src: js, alt: "JavaScript", title: "JavaScript ES6+", size: 48 },
   mobileDesign: { src: mobiledesign, alt: "Mobile Design", title: "Mobile Design", size: 48 },
   next: { src: next, alt: "Next.js", title: "Next.js", size: 48 },
   nodejs: { src: nodejs, alt: "Node.js", title: "Node.js", size: 48 },
+  openai: { src: openai, alt: "OpenAI", title: "OpenAI", size: 48 },
   payu: { src: payu, alt: "Integración de Pagos", title: "Integración de Pagos", size: 48 },
   postgresql: { src: postgresql, alt: "PostgreSQL", title: "PostgreSQL", size: 48 },
   pusher: { src: pusher, alt: "Pusher", title: "Pusher", size: 48 },
@@ -58,6 +67,7 @@ export const stackIcons = {
   swiper: { src: swiperjs, alt: "Swiper.js", title: "Swiper.js", size: 48 },
   tailwind: { src: tailwind, alt: "Tailwind CSS", title: "Tailwind CSS", size: 48 },
   typescript: { src: ts, alt: "TypeScript", title: "TypeScript", size: 48 },
+  uiverse: { src: uiverse, alt: "Uiverse", title: "Uiverse", size: 48 },
   vite: { src: vite, alt: "Vite", title: "Vite", size: 48 },
   zustand: { src: zustand, alt: "Zustand", title: "Zustand", size: 48 },
 };
