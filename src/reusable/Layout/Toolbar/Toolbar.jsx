73b9d8147280d16e88df767ce883e723db7418a0
EsneiderCdD <ecd.97@hotmail.com>
2026-09-07 16:29:16 -0500

    feat(clean)

--- src/reusable/Layout/Toolbar/Toolbar.jsx
+++ src/reusable/Layout/Toolbar/Toolbar.jsx
@@ -1,6 +1,5 @@
 import styles from "./styles/Toolbar.module.css";
 import SocialButtons from "./components/SocialButtons";
-import SearchBar from "./components/SearchBar";
 import ProposalButton from "./components/ProposalButton";
 
 function Toolbar() {
@@ -8,7 +7,6 @@ function Toolbar() {
         <div className={styles.header}>
             <ProposalButton />
             <SocialButtons />
-            <SearchBar />
         </div>
     );
 }
